c010c07b2f313144b15f8bff9ffd267bc9ac1d84
warith <warith.ad02@gmail.com>
2022-12-23 20:03:19 +0100

    Create styled components files for styling sections and form

---
+++ src/components/cv-form/style/SectionStyle.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const SectionStyle = styled.section`
+    display: flex;
+    flex-direction: column;
+    font-family: Arial;
+    gap: 1rem;
+    padding: 0 2rem; 
+`
+export default SectionStyle;
