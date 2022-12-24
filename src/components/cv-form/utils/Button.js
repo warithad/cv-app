698c7f5a5f183850504aa1b77eb1170448298bec
warith <warith.ad02@gmail.com>
2022-12-25 00:35:28 +0100

    Move Button.js to utils folder, add react icon to button and add styling to button component

---
+++ src/components/cv-form/utils/Button.js
@@ -0,0 +1,26 @@
+import styled from "styled-components"
+import { AiOutlinePlus } from 'react-icons/ai'
+const Button =({title, onClick})=>{
+    return (
+        <ButtonContainer 
+            onClick={onClick}>{title} 
+            <AiOutlinePlus/>
+        </ButtonContainer>
+    )
+}
+
+const ButtonContainer = styled.button`
+    height: 40px;
+    border: none;
+    padding: 10px;
+    font-size: 1.2rem;
+    border-radius: 5px;
+    background-color: #1e293b;
+    color: white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: .5rem;
+`
+
+export default Button;
