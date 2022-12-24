a5baee38841547996ea58bb4e3232e27a9260358
warith <warith.ad02@gmail.com>
2022-12-25 00:08:42 +0100

    Create parent components for experiences and educations components

---
+++ src/components/cv-form/EducationsContainer.js
@@ -0,0 +1,13 @@
+import Button from "./Button"
+
+const EducationsContainer =()=>{
+    return (
+        <>
+            <h1>Education</h1>
+            <></>
+            <Button title='Add'/>
+        </>
+    )
+}
+
+export default EducationsContainer;
---
+++ src/components/cv-form/ExperiencesContainer.js
@@ -0,0 +1,14 @@
+import Button from "./Button"
+
+const ExperiencesContainer = () =>{
+
+    return (
+        <>
+            <h1>Experience</h1>
+            <></>
+            <Button title='Add'></Button>
+        </>
+    )
+}
+
+export default ExperiencesContainer;
